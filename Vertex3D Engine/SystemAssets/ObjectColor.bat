e70fe08cf3044b6a63f3d6e2651fcb7abbcb13b2
Hex_Studios_Team <hexstudiosteam@gmail.com>
2016-03-26 19:42:25 -0500

    More scripts >:)

---
+++ Vertex3D Engine/SystemAssets/ObjectColor.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo SYNTAX:
+echo 0.0 0.0 0.0 (This is an example of the color black)
+echo Always make your values a decimal
+echo The first number is your red value, the second is green, and the third is blue
+
+set /p NewColor=New RGB Value: 
+
+cd "c://Program Files/Vertex3D/Vertex3DModels/Engine"
+
+echo %NewColor% > matColor.enginesys
---
+++ Vertex3D Engine/SystemAssets/SkyColor.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo SYNTAX:
+echo 0.0 0.0 0.0 1.0 (This is an example of the color black with an alpha of 1.0)
+echo Always make your values a decimal
+echo The first number is your red value, the second is green, the third is blue, and the last is alpha (Transparencey)
+
+set /p NewColor=New RGBA Value: 
+
+cd "c://Program Files/Vertex3D/Vertex3DModels/Engine"
+
+echo %NewColor% > SkyboxColor.enginesys
