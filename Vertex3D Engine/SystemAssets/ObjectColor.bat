5692bcc11c8fe86f3f31eba77fca6e13032c0335
Hex_Studios_Team <hexstudiosteam@gmail.com>
2016-03-26 19:44:50 -0500

    Changed em

--- Vertex3D Engine/SystemAssets/ObjectColor.bat
+++ Vertex3D Engine/SystemAssets/ObjectColor.bat
@@ -5,8 +5,11 @@ echo 0.0 0.0 0.0 (This is an example of the color black)
 echo Always make your values a decimal
 echo The first number is your red value, the second is green, and the third is blue
 
+:main
 set /p NewColor=New RGB Value: 
 
 cd "c://Program Files/Vertex3D/Vertex3DModels/Engine"
 
 echo %NewColor% > matColor.enginesys
+
+goto main
